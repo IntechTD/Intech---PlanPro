8aea8599c0f199447006bcb14a6b0cbffab92fb9
IntechTD <ALESSIO.DEGIACOBBE@GMAIL.COM>
2025-06-04 08:59:00 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Index from './pages/Index'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
-)
+);
